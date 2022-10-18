37f6da6b9a14cf6ebd787e739a15d75a6708f72a
lewismunene <munene.lewis@students.jkuat.ac.ke>
2022-10-18 12:31:49 +0300

    updating redirect link

--- definitions.php
+++ definitions.php
@@ -1,7 +1,7 @@
 ﻿<?php
     define('fbAppId', '786378435962833');
     define('fbAppSecret', '047a38eca55e632dfc2704a9a884821f');
-    define('fbRedirectUri', 'http://localhost:8000/getUserAccess.php'); //http://localhost:8000/getUserAccess.php
+    define('fbRedirectUri', 'https://example.com'); //http://localhost:8000/getUserAccess.php
     define('fbGraphVersion', 'v15.0');
     define('fbGraphDomain', 'https://graph.facebook.com/');
     define('accessCodeTxt', 'accessCode.txt');
